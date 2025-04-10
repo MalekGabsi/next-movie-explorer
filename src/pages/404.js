670fca6da866d98006d459bd41b42130dd98baf5
MalekGabsi <malekgabsi42@gmail.com>
2025-04-10 13:55:02 +0100

    new build

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useRouter } from 'next/router'; 
+import Link from 'next/link';
 
 const NotFound = () => {
   const router = useRouter(); 
@@ -9,12 +10,14 @@ const NotFound = () => {
   }, [router]);
 
   return (
-    <div className="bg-gray-900 min-h-screen text-white flex flex-col items-center justify-center m-0 pt-10">
+    <div className="bg-gray-900 min-h-screen text-white flex flex-col items-center justify-center p-10">
       <h1 className="text-6xl font-bold">404</h1>
-      <p className="text-lg mt-4">Oops! The page you're looking for doesn't exist.</p>
-      <a href="/" className="mt-8 px-6 py-2 bg-red-500 rounded-md text-white hover:bg-red-600">
-        Go back to Home
-      </a>
+      <p className="text-lg mt-4">Oops! The page you are looking for does not exist.</p>
+      <Link href="/">
+        <a className="mt-8 px-6 py-2 bg-red-500 rounded-md text-white hover:bg-red-600 transition">
+          Go back to Home
+        </a>
+      </Link>
     </div>
   );
 };
