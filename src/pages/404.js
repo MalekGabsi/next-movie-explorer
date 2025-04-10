e50a4880fc40e17041b2ddd312067b141264008a
MalekGabsi <malekgabsi42@gmail.com>
2025-04-10 14:09:15 +0100

    new build

--- src/pages/404.js
+++ src/pages/404.js
@@ -10,13 +10,11 @@ const NotFound = () => {
   }, [router]);
 
   return (
-    <div className="bg-gray-900 min-h-screen text-white flex flex-col items-center justify-center p-10">
+    <div className="bg-gray-900 min-h-screen text-white flex flex-col items-center justify-center m-0 pt-10">
       <h1 className="text-6xl font-bold">404</h1>
-      <p className="text-lg mt-4">Oops! The page you are looking for does not exist.</p>
-      <Link href="/">
-        <a className="mt-8 px-6 py-2 bg-red-500 rounded-md text-white hover:bg-red-600 transition">
-          Go back to Home
-        </a>
+      <p className="text-lg mt-4">Oops! The page you are looking for do not exist.</p>
+      <Link href="/" className="mt-8 px-6 py-2 bg-red-500 rounded-md text-white hover:bg-red-600">
+        Go back to Home
       </Link>
     </div>
   );
